d70390ac7e44186e09b8e22d57a4a6d1249e0c43
Jan Kacaba <jan.kacaba@gmail.com>
2023-09-08 11:16:01 +0200

    refactor main and _cli

--- src/rapi/_cli.py
+++ src/rapi/_cli.py
@@ -1,16 +1,10 @@
-import os
-
 from rapi import CFG, Client
 from rapi._command import commands
-
-__all__ = "main"
-
 
 def main() -> None:
     Cfg = CFG()
     Cfg.cfg_runtime_set_defaults()
     commands(Cfg)
-
 
 if __name__ == "__main__":
     main()
