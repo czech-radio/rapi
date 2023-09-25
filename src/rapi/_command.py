45fd60e37fe82cdc23a828cd076b505dee3beaff
Jan Kacaba <jan.kacaba@gmail.com>
2023-09-25 18:32:17 +0200

    simplified commands and subcommands list

--- src/rapi/_command.py
+++ src/rapi/_command.py
@@ -1,18 +1,17 @@
-import logging
 import sys
 
 from rapi import __version__
 from rapi._client import Client
 from rapi.config._config import Config
-from rapi.helpers import helpers
-from rapi.helpers import _logger
-from rapi.helpers._logger import log_stderr as loge
+from rapi.helpers import _logger, helpers
+# from rapi.helpers._logger import log_stderr as loge
 from rapi.helpers._logger import log_stdout as logo
 
 
 def commands(cfg: Config) -> None:
     try:
         commands_list(cfg)
+        subcommand_list(cfg)
     except SystemExit as err:
         if err.code == 0:
             return
@@ -23,53 +22,44 @@ def commands(cfg: Config) -> None:
 
 
 def commands_list(cfg: Config) -> None:
-    getvar = cfg.runtime_get
-
-    # test logs
-    run = getvar(["test", "logs"])
-    test_logs(run)
-
-    # print version
-    print_version(cfg)
-
-    # debug cfg
-    run = getvar(["debug", "cfg"])
-    debug_cfg(run, cfg)
-
-    # subcommands
-    subc = getvar(["subcommand"], None)
-    if subc is None:
-        return None
-    else:
-        logo.info(f"running command: {subc}")
-
-    if "station" == subc:
-        croapp = Client(cfg)
-        # print(vars(ap))
-        guid = croapp.get_station_guid("11")
-        print(guid)
-
-    if "show" == subc:
-        pass
+    cmd_test_logs(cfg)
+    cmd_print_version(cfg)
+    cmd_debug_cfg(cfg)
     return None
 
 
-def print_version(cfg: Config) -> None:
+def subcommand_list(cfg: Config) -> None:
+    subcommand = cfg.runtime_get(["subcommand"], None)
+    if subcommand is None:
+        return None
+    logo.info(f"running command: {subcommand}")
+    # NOTE: this can be simplified to dict or even to a list
+    match subcommand:
+        case "station_guid":
+            subcmd_station_guid(cfg)
+    return None
+
+
+def subcmd_station_guid(cfg: Config):
+    croapp = Client(cfg)
+    guid = croapp.get_station_guid("11")
+    print(guid)
+
+
+def cmd_print_version(cfg: Config) -> None:
     if cfg.runtime_get(["version"]):
         print(__version__)
         sys.exit(0)
 
 
-def test_logs(run: bool):
-    if run is False:
-        return
-    _logger.test_logs()
-    sys.exit(0)
+def cmd_test_logs(cfg: Config) -> None:
+    if cfg.runtime_get(["test", "logs"]):
+        _logger.test_logs()
+        sys.exit(0)
 
 
-def debug_cfg(run: bool, cfg: Config):
-    if run is False:
-        return
-    data = cfg.cfg_runtime
-    helpers.pp(data)
-    sys.exit(0)
+def cmd_debug_cfg(cfg: Config):
+    if cfg.runtime_get(["debug", "cfg"]):
+        data = cfg.cfg_runtime
+        helpers.pp(data)
+        sys.exit(0)
